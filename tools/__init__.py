60c3993573b5a3db9e34fb737fd2aa80a57802fb
yo-aka-gene <yujiokano@keio.jp>
2023-10-26 22:33:09 +0900

    :sparkles: add kwargs

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,6 +1,8 @@
 from ._data_loader import SuematsuData
+from ._preference import kwarg_savefig
 
 
 __all__ = [
     "SuematsuData",
+    "kwarg_savefig"
 ]
---
+++ tools/_preference.py
@@ -0,0 +1,7 @@
+kwarg_savefig = {
+    "facecolor": "white",
+    "dpi": 300,
+    "bbox_inches": "tight",
+    "pad_inches": 0.05,
+    "transparent": True,
+}
