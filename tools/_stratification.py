98d6db6f4f1f14f932549dadf7d2f7a0979eaeff
yo-aka-gene <yujiokano@keio.jp>
2023-10-28 03:21:42 +0900

    :recycle::sparkles: add go_venn and top_go_venn

--- tools/_stratification.py
+++ tools/_stratification.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
+from matplotlib_venn import venn3
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from ._cohens_d import gene_selection
 from ._data_loader import SuematsuData
 from ._go import gprofiler
-from ._preference import kwarg_savefig
+from ._preference import kwarg_savefig, venn3_palette_alias
 
 
 class StratifiedGOAnalysis:
@@ -71,23 +72,32 @@ class StratifiedGOAnalysis:
         ) -> None:
         self.data = self.data_setter(data=data, d=d, split_by_days=split_by_days)
         self.palette = {"up": palettes[0], "down": palettes[1]}
+        self.venn3_palette = "palette1" if split_by_days else "palette2"
         self.title = self.title_setter(split_by_days=split_by_days)
         self.out = out
+        self.result = {"up": {}, "down": {}}
+        for category in self.data:
+            for subcategory in self.data[category]:
+                self.result[category] = {
+                    **self.result[category], 
+                    subcategory: gprofiler(self.data[category][subcategory])
+                }
 
 
     def go_plot(
         self,
-        data: pd.DataFrame,
+        category: str,
+        subcategor: str,
         ax: plt.Axes = None,
         top: int = None,
         palette: str = None
     ):
+        data = self.result[category][subcategor]
         top = min(len(data), top) if isinstance(top, int) else len(data)
         if ax is None:
             ysize = (lambda x: max(5, int(x / 5)))(top)
             _, ax = plt.subplots(figsize=(5, ysize))
-        
-        res = gprofiler(data).iloc[:top, :].iloc[::-1, :]
+        res = data.iloc[:top, :].iloc[::-1, :]
         res = pd.DataFrame({
             "term_name": res.term_name,
             "$-\log_{10}Pval.$": -np.log10(res.p_value),
@@ -117,10 +127,10 @@ class StratifiedGOAnalysis:
 
 
     def enrichment_analysis(
-            self,
-            figsize: tuple = None,
-            top: int = 30
-        ):
+        self,
+        figsize: tuple = None,
+        top: int = 30
+    ):
         for category in self.data:
             for subcategory in self.data[category]:
                 fig, ax = plt.subplots(figsize=figsize)
@@ -138,10 +148,10 @@ class StratifiedGOAnalysis:
 
 
     def silent_enrichment_analysis(
-            self,
-            figsize: tuple = None,
-            top: int = 30
-        ):
+        self,
+        figsize: tuple = None,
+        top: int = 30
+    ):
         for category in self.data:
             for subcategory in self.data[category]:
                 fig, ax = plt.subplots(figsize=figsize)
@@ -159,9 +169,70 @@ class StratifiedGOAnalysis:
                 self.close()
 
 
+    def go_venn_base(
+        self,
+        category: str,
+        top: int = None,
+        ax: plt.Axes = None,
+        palette: str = None
+    ) -> None:
+        if ax is None:
+            _, ax = plt.subplots()
+        database = self.result[category]
+        top = 100000 if top is None else top
+        res = pd.concat(
+            [
+                pd.DataFrame(
+                    (10 ** i) * np.ones(len(database[key])), 
+                    index=database[key].term_name
+                ).iloc[:top, :] for i, key in enumerate(database)
+            ],
+            axis=1
+        ).fillna(0).sum(axis=1).value_counts().sort_index()
+        v = venn3(
+            subsets=[res[i] if i in res.index else 0 for i in [1, 10, 11, 100, 101, 110, 111]],
+            set_labels = database.keys(),
+            ax=ax
+        )
+        palette = venn3_palette_alias[2] if palette is None else venn3_palette_alias[palette]
+        [v.get_patch_by_id(p).set_color(c) for p, c in palette.items() if v.get_patch_by_id(p) is not None]
+
+
+    def top_go_venn(
+        self,
+        layout: tuple = (1, 2),
+        figsize: tuple = (6, 3),
+        top: int = 30
+    ) -> None:
+        fig, ax = plt.subplots(*layout, figsize=figsize)
+        self.go_venn_base(category="up", top=top, ax=ax[0], palette=self.venn3_palette)
+        self.go_venn_base(category="down", top=top, ax=ax[1], palette=self.venn3_palette)
+        [a.set(title=f"{k}regulated GO terms (top{top})") for a, k in zip(ax, self.result)]
+        suffix = "_".join(list(self.result["up"].keys())[::2])
+        fig.savefig(f"{self.out}/go_venn_top{top}_{suffix}.png", **kwarg_savefig)
+
+
+    def go_venn(
+        self,
+        layout: tuple = (1, 2),
+        figsize: tuple = (6, 3),
+        top: int = None
+    ) -> None:
+        fig, ax = plt.subplots(*layout, figsize=figsize)
+        self.go_venn_base(category="up", top=top, ax=ax[0], palette=self.venn3_palette)
+        self.go_venn_base(category="down", top=top, ax=ax[1], palette=self.venn3_palette)
+        [a.set(title=f"{k}regulated GO terms (all)") for a, k in zip(ax, self.result)]
+        suffix = "_".join(list(self.result["up"].keys())[::2])
+        fig.savefig(f"{self.out}/go_venn_all_{suffix}.png", **kwarg_savefig)
+
+
     def pipeline(
         self, 
-        pipe: list = ["silent_enrichment_analysis"],
+        pipe: list = [
+            "silent_enrichment_analysis",
+            "top_go_venn",
+            "go_venn"
+        ],
         close: bool = False
     ) -> None:
         for operation in pipe:
