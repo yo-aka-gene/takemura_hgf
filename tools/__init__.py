d953244b15157d5fee016ae3cf73a914ae712cd4
yo-aka-gene <yujiokano@keio.jp>
2023-10-27 19:01:24 +0900

    :sparkles: add get_go

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,6 +1,6 @@
 from ._cohens_d import cohens_d, gene_selection
 from ._data_loader import SuematsuData
-from ._go import go, gprofiler
+from ._go import go, gprofiler, get_go
 from ._preference import kwarg_savefig, eda_longitudal_args, sgoa_longitudal_args
 from ._eda import EDA
 from ._stratification import StratifiedGOAnalysis
@@ -13,6 +13,7 @@ __all__ = [
     "eda_longitudal_args",
     "kwarg_savefig",
     "sgoa_longitudal_args",
+    "get_go",
     "go",
     "gprofiler",
     "cohens_d",
--- tools/_go.py
+++ tools/_go.py
@@ -7,11 +7,8 @@ import os
 def go(data: pd.DataFrame):
     tempdir = TemporaryDirectory()
     data.to_csv(f"{tempdir.name}/data.csv", index=True)
-
     cmd = f"Rscript {os.path.dirname(__file__)}/_go_pipeline.R -t {tempdir.name}"
-    subprocess.call(
-        cmd.split()
-    )
+    subprocess.call(cmd.split())
     ret = pd.read_csv(f"{tempdir.name}/enrichment.csv", index_col=0)
     tempdir.cleanup()
     return ret
@@ -20,11 +17,18 @@ def go(data: pd.DataFrame):
 def gprofiler(data: pd.DataFrame):
     tempdir = TemporaryDirectory()
     data.to_csv(f"{tempdir.name}/data.csv", index=True)
-
     cmd = f"Rscript {os.path.dirname(__file__)}/_gprofiler_pipeline.R -t {tempdir.name}"
-    subprocess.call(
-        cmd.split()
-    )
+    subprocess.call(cmd.split())
     ret = pd.read_csv(f"{tempdir.name}/enrichment.csv", index_col=0)
+    tempdir.cleanup()
+    return ret
+
+
+def get_go(data: pd.DataFrame):
+    tempdir = TemporaryDirectory()
+    data.to_csv(f"{tempdir.name}/data.csv", index=True)
+    cmd = f"Rscript {os.path.dirname(__file__)}/_get_go.R -t {tempdir.name}"
+    subprocess.call(cmd.split())
+    ret = pd.read_csv(f"{tempdir.name}/gomatrix.csv", index_col=0)
     tempdir.cleanup()
     return ret
