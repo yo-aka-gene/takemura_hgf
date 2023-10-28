0927bb28d865f78cbb752a631ae545a4f7735ad8
yo-aka-gene <yujiokano@keio.jp>
2023-10-28 19:26:27 +0900

    :sparkles: add go2gene_barplot and other funcs

--- tools/_stratification.py
+++ tools/_stratification.py
@@ -5,7 +5,7 @@ import pandas as pd
 import seaborn as sns
 from ._cohens_d import gene_selection
 from ._data_loader import SuematsuData
-from ._go import gprofiler
+from ._go import gprofiler, get_go
 from ._preference import kwarg_savefig, venn3_palette_alias
 
 
@@ -83,6 +83,7 @@ class StratifiedGOAnalysis:
         self.data = self.data_setter(data=data, d=d, split_by_days=split_by_days)
         self.palette = {"up": palettes[0], "down": palettes[1]}
         self.venn3_palette = "palette1" if split_by_days else "palette2"
+        self.barplot_palette = ("C1", "C2", "C0") if split_by_days else ("y", "grey", "m")
         self.title = self.title_setter(split_by_days=split_by_days)
         self.out = out
         self.result = {"up": {}, "down": {}}
@@ -238,12 +239,138 @@ class StratifiedGOAnalysis:
         fig.savefig(f"{self.out}/go_venn_all_{suffix}.png", **kwarg_savefig)
 
 
+    def go2gene_base(
+        self,
+        category: str,
+        top: int = None,
+        filter_id: int = None
+    ) -> None:
+        database = self.result[category]
+        gene_data = self.data[category]
+        ref = pd.concat([
+            v.loc[:, ["term_id", "term_name"]].set_index("term_name") for v in database.values()
+        ]).drop_duplicates()
+        top = 100000 if top is None else top
+        res = pd.concat(
+            [
+                pd.DataFrame(
+                    (10 ** i) * np.ones(len(database[key])), 
+                    index=database[key].term_name
+                ).iloc[:top, :] for i, key in enumerate(database)
+            ],
+            axis=1
+        ).fillna(0).sum(axis=1)
+        query = ref.loc[res.index, :] if filter_id is None else ref.loc[res[res == filter_id].index, :]
+        hit = get_go(query).set_index("SYMBOL")
+        gene_list = {
+            k: [gene.capitalize() for gene in v.index.str.upper() if gene in hit.index] for k, v in gene_data.items()
+        }
+        hit_dict = {
+            k: hit.loc[
+                [gene.upper() for gene in v],
+                ["GOALL", "ONTOLOGYALL"]
+            ].reset_index().drop_duplicates() for k, v in gene_list.items()
+        }
+        return gene_list, query, hit_dict
+
+
+    def go2gene_name_viz_base(
+        query: pd.DataFrame,
+        hit_dict: dict,
+        ax: plt.Axes = None,
+        plotsize: float = 4,
+        hspace: float = .4
+    ) -> tuple:
+        # tentative
+        n_cols = int(np.ceil(np.sqrt(len(query))))
+        n_rows = int(np.ceil(len(query) / n_cols))
+        fig, ax = plt.subplots(n_rows, n_cols, figsize=(plotsize * n_cols, plotsize * n_rows))
+        for i in range(ax.size):
+            ax[i // n_cols, i % n_cols].axis("off") if i >= len(query) else None
+        fig.subplots_adjust(hspace=hspace)
+        for i, term in enumerate(query.index):
+            a = ax[i // n_cols, i % n_cols]
+            dat = {
+                k: d[d.GOALL == query.term_id[i]].reset_index().SYMBOL for k, d in hit_dict.items()
+            }
+            for x, k in enumerate(dat):
+                for y, name in zip(dat[k].index, dat[k]):
+                    a.text(
+                        x, y, name, horizontalalignment="center", 
+                        verticalalignment="center", c=".2"
+                    )
+            a.set_xticks(np.arange(len(dat)), dat.keys())
+            a.set_xlim([-.5, len(dat) - .5])
+            a.set_ylim([-.5, max([len(d) for d in dat.values()]) - .5])
+            a.tick_params(left=False, labelleft=False)
+            a.set(title=term)
+        return fig, ax
+
+
+    def go2gene_n_viz_base(
+        query: pd.DataFrame,
+        hit_dict: dict,
+        category: str = None,
+        plotsize: float = 3,
+        wspace: float = .3,
+        hspace: float = .2,
+        palette: tuple = ("y", "grey", "m")
+    ) -> tuple:
+        n_cols = int(np.ceil(np.sqrt(len(query))))
+        n_rows = int(np.ceil(len(query) / n_cols))
+        fig, ax = plt.subplots(n_rows, n_cols, figsize=(plotsize * n_cols, plotsize * n_rows))
+        ax = ax if len(query) > 2 else np.array([ax.tolist()])
+        for i in range(ax.size):
+            ax[i // n_cols, i % n_cols].axis("off") if i >= len(query) else None
+        fig.subplots_adjust(wspace=wspace, hspace=hspace)
+        for i, term in enumerate(query.index):
+            a = ax[i // n_cols, i % n_cols]
+            dat = pd.DataFrame({
+                k: [len(d[d.GOALL == query.term_id[i]])] for k, d in hit_dict.items()
+            }).T
+            sns.barplot(data=dat.reset_index(), x="index", y=0, palette=palette, alpha=.5, ax=a)
+            for x, v in enumerate(dat.iloc[:, 0].values):
+                a.text(
+                    x, v / 2, v, horizontalalignment="center", 
+                    verticalalignment="center", c=".2"
+                )
+            a.set_xticks(np.arange(len(dat)), dat.index)
+            a.set_xlim([-.5, len(dat) - .5])
+            a.set(
+                title=fmt_go_terms(term), 
+                ylabel="Num. of genes" if category is None else f"Num. of {category}. genes", 
+                xlabel=""
+            )
+        return fig, ax
+
+    def go2gene_barplot(
+        self,
+        top: int = 30,
+        filter_id: int = 111,
+        plotsize: float = 3,
+        wspace: float = .3,
+        hspace: float = .3,
+    ) -> None:
+        for category in self.data:
+            _, query, hit_dict = self.go2gene_base(category=category, top=top, filter_id=filter_id)
+            fig, _ = self.go2gene_n_viz_base(
+                query=query, hit_dict=hit_dict, plotsize=plotsize,
+                wspace=wspace, hspace=hspace, palette=self.barplot_palette
+            )
+            suffix = "_".join(list(self.result["up"].keys())[::2])
+            fig.savefig(
+                f"{self.out}/go_barplot_{category}_{filter_id}_top{top}_{suffix}.png",
+                **kwarg_savefig
+            )
+
+
     def pipeline(
         self, 
         pipe: list = [
             "silent_enrichment_analysis",
             "top_go_venn",
-            "go_venn"
+            "go_venn",
+            "go2gene_barplot"
         ],
         close: bool = False
     ) -> None:
