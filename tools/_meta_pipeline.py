d5958370e74b6e93a09d26c793b2e90b3aa3b0e5
yo-aka-gene <yujiokano@keio.jp>
2023-10-29 18:49:03 +0900

    :recycle::sparkles: add adgile arg in pipeline

--- tools/_meta_pipeline.py
+++ tools/_meta_pipeline.py
@@ -1,3 +1,7 @@
-def metapipeline(pipelines: list, close: bool = True):
+def metapipeline(
+    pipelines: list,
+    close: bool = True,
+    adgile: bool =True
+) -> None:
     for p in pipelines:
-        p.pipeline(close=close)
+        p.pipeline(close=close, adgile=adgile)
