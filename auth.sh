9d3690a84a5aeb2f7272b0f2515d34423dc12a2c
yo-aka-gene <yujiokano@keio.jp>
2023-10-29 21:11:16 +0900

    :recycle: assign docker-compose.yml as arg for sed and git update-index for safety

--- auth.sh
+++ auth.sh
@@ -2,5 +2,5 @@
 
 get_id=$(id)
 nb_id=${get_id[@]:4:3}
-sed -i '' -e s/YOUR_ID/${nb_id}/ $1
-git update-index --assume-unchanged $1
+sed -i '' -e s/YOUR_ID/${nb_id}/ $(dirname $0)/docker-compose.yml
+git update-index --assume-unchanged $(dirname $0)/docker-compose.yml
