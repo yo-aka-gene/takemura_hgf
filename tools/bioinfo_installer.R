8ecb7c22e140aef7aa7f65cf53957f6794cb0512
yo-aka-gene <yujiokano@keio.jp>
2023-11-18 04:48:05 +0900

    :wrench::recycle: add repos and org.Mm.eg.db

--- tools/bioinfo_installer.R
+++ tools/bioinfo_installer.R
@@ -1,9 +1,10 @@
 ### bioinfo_installer.R ###
 
-install.packages("BiocManager")
+install.packages("BiocManager", repos="http://cran.ism.ac.jp/")
 BiocManager::install("clusterProfiler")
 BiocManager::install("AnnotationDbi")
 BiocManager::install("org.Hs.eg.db")
+BiocManager::install("org.Mm.eg.db")
 BiocManager::install("GO.db")
-install.packages("optparse")
-install.packages("gprofiler2")
+install.packages("optparse", repos="http://cran.ism.ac.jp/")
+install.packages("gprofiler2", repos="http://cran.ism.ac.jp/")
