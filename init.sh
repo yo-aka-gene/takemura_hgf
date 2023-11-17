4872e1342c566ef44b97cf1e585469d4f8fb5485
yo-aka-gene <yujiokano@keio.jp>
2023-11-18 04:18:35 +0900

    :wrench::recycle: add code to update pip automatically

--- init.sh
+++ init.sh
@@ -2,8 +2,10 @@
 
 sh auth.sh docker-compose.yml
 docker compose up -d
-sh ./lib.sh
 docker start $((basename $PWD) | tr '[A-Z]' '[a-z]')-jupyterlab-1
+docker exec $((basename $PWD) | tr '[A-Z]' '[a-z]')-jupyterlab-1 sudo pip -U pip
+sh ./lib.sh
+docker exec $((basename $PWD) | tr '[A-Z]' '[a-z]')-jupyterlab-1 sudo pip -U pip
 docker exec $((basename $PWD) | tr '[A-Z]' '[a-z]')-jupyterlab-1 Rscript ./tools/bioinfo_installer.R
 sh ./writelib.sh
 open http://localhost:8008
