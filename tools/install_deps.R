c4fb224e12947256565ddc17acf79b62f85a9791
yo-aka-gene <yujiokano@keio.jp>
2023-11-18 04:21:37 +0900

    :recycle: add repos

--- tools/install_deps.R
+++ tools/install_deps.R
@@ -6,9 +6,9 @@ current_pkg <- installed.packages()
 
 for(package in rownames(deps)) {
     if (package %notin% rownames(current_pkg)) {
-        remotes::install_version(package, version = deps[package,"Version"])
+        remotes::install_version(package, version = deps[package,"Version"], repos="http://cran.ism.ac.jp/")
     } else if (deps[package,"Version"] != current_pkg[package,"Version"]) {
-        remotes::install_version(package, version = deps[package,"Version"])
+        remotes::install_version(package, version = deps[package,"Version"], repos="http://cran.ism.ac.jp/")
     } else {
         print(sprintf("Requirement is already satisfied: %s (version==%s)", package, deps[package,"Version"]))
     }
