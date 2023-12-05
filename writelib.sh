eb6f24bd9bba0b611bb86ab26b114bf9de808b2d
yo-aka-gene <yujiokano@keio.jp>
2023-12-05 23:37:05 +0900

    :wrench: add requirements.txt for sphinx

--- writelib.sh
+++ writelib.sh
@@ -2,3 +2,4 @@
 
 docker exec -it $((basename $PWD) | tr '[A-Z]' '[a-z]')-jupyterlab-1 sudo pip list --format=freeze > ./tools/requirements_py.txt
 docker exec $((basename $PWD) | tr '[A-Z]' '[a-z]')-jupyterlab-1 Rscript ./tools/export_deps.R
+grep -e sphinx -e doc -e myst-parser ./tools/requirements_py.txt > docs/requirements.txt
