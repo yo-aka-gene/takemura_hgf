5efa0f43d89ed82aaaa27aaebed77bb6695cbc70
yo-aka-gene <yujiokano@keio.jp>
2023-12-05 23:53:44 +0900

    :bug: add myst-parser

--- writelib.sh
+++ writelib.sh
@@ -3,5 +3,5 @@
 docker exec -it $((basename $PWD) | tr '[A-Z]' '[a-z]')-jupyterlab-1 sudo pip list --format=freeze > ./tools/requirements_py.txt
 docker exec $((basename $PWD) | tr '[A-Z]' '[a-z]')-jupyterlab-1 Rscript ./tools/export_deps.R
 grep \
--e sphinx -e numpy -e pand -e matplotlib -e seaborn -e sci \
+-e sphinx -e numpy -e pand -e matplotlib -e seaborn -e sci -e myst-parser \
 ./tools/requirements_py.txt > docs/requirements.txt
