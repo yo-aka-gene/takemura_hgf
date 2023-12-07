4ccd4f99af282618eedd914cc3b0d83a23304b92
yo-aka-gene <yujiokano@keio.jp>
2023-12-08 06:26:38 +0900

    :recycle::bug: fix bug in designating path

--- transfer_docs.sh
+++ transfer_docs.sh
@@ -3,8 +3,10 @@
 # This script is for the author to protect docs and analysis outputs private before acceptance
 # We will move docs in a privae repository tentatively during revision
 
+NB_NAME = $(basename $1)
+
 mv $1 ~/Desktop/hgf_manuscript/hgf_docs
-cp $(basename $PWD)/docs/README.ipynb $1
+cp $(basename $PWD)/docs/jupyternb/README.ipynb $1
 git add $1
 git commit -m ":construction::books: add tentative doc ($(basename $1))"
 cd ~/Desktop/hgf_manuscript/hgf_docs
