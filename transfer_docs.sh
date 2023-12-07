e4505715dda5c3cd49cdf49b4c6004241c33107a
yo-aka-gene <yujiokano@keio.jp>
2023-12-08 06:55:26 +0900

    :bug: fix bugs

--- transfer_docs.sh
+++ transfer_docs.sh
@@ -3,8 +3,8 @@
 # This script is for the author to protect docs and analysis outputs private before acceptance
 # We will move docs in a privae repository tentatively during revision
 
-NB_NAME = $(basename $1)
-HGF_DIR = $PWD
+NB_NAME="$(basename $1)"
+HGF_DIR=$PWD
 
 mv $1 ~/Desktop/hgf_manuscript/hgf_docs
 cd ~/Desktop/hgf_manuscript/hgf_docs
@@ -13,4 +13,4 @@ git commit -m ":sparkles: add $NB_NAME"
 git push origin main
 
 cd $HGF_DIR
-cp $(basename $PWD)/docs/jupyternb/README.ipynb $(basename $PWD)/docs/jupyternb/$NB_NAME
+cp docs/jupyternb/README.ipynb docs/jupyternb/$NB_NAME
