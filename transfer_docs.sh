03ab5a3abc267c1f5b496624137e3fda74ac00b5
yo-aka-gene <yujiokano@keio.jp>
2023-12-08 06:35:32 +0900

    :recycle::bug: fix bug in designating path

--- transfer_docs.sh
+++ transfer_docs.sh
@@ -4,12 +4,13 @@
 # We will move docs in a privae repository tentatively during revision
 
 NB_NAME = $(basename $1)
+HGF_DIR = $PWD
 
 mv $1 ~/Desktop/hgf_manuscript/hgf_docs
-cp $(basename $PWD)/docs/jupyternb/README.ipynb $(basename $PWD)/docs/jupyternb/$NB_NAME
-git add $(basename $PWD)/docs/jupyternb/$NB_NAME
-git commit -m ":construction::books: add tentative doc ($NB_NAME)"
 cd ~/Desktop/hgf_manuscript/hgf_docs
 git add ~/Desktop/hgf_manuscript/hgf_docs/$NB_NAME
 git commit -m ":sparkles: add $NB_NAME"
 git push origin main
+
+cd $HGF_DIR
+cp $(basename $PWD)/docs/jupyternb/README.ipynb $(basename $PWD)/docs/jupyternb/$NB_NAME
