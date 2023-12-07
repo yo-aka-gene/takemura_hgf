57ed350f78a6d73b38ff830d1f80d283da2fef8e
yo-aka-gene <yujiokano@keio.jp>
2023-12-08 06:16:59 +0900

    :wrench: add script to transfer docs to private repo

---
+++ transfer_docs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# This script is for the author to protect docs and analysis outputs private before acceptance
+# We will move docs in a privae repository tentatively during revision
+
+mv $1 ~/Desktop/hgf_manuscript/hgf_docs
+cp $(basename $PWD)/docs/README.ipynb $1
+git add $1
+git commit -m ":construction::books: add tentative doc ($(basename $1))"
+cd ~/Desktop/hgf_manuscript/hgf_docs
+git add ~/Desktop/hgf_manuscript/hgf_docs/$(basename $1)
+git commit -m ":sparkles: add $(basename $1)"
+git push origin main
